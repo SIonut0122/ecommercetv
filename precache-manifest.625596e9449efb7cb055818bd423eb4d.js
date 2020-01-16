21cb9b6b846466249939d41b13413afa1d9a9dcc
SIonut0122 <sionut0122@yahoo.com>
2020-01-16 17:17:01 +0100

    Updates

---
+++ precache-manifest.625596e9449efb7cb055818bd423eb4d.js
@@ -0,0 +1,370 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "083a23ae29abf8ad8fe3000a5ce791f9",
+    "url": "/ecommercetv/index.html"
+  },
+  {
+    "revision": "f9f938f4c857dbb84110",
+    "url": "/ecommercetv/static/css/main.e4cafc83.chunk.css"
+  },
+  {
+    "revision": "fbe31eec44071af96872",
+    "url": "/ecommercetv/static/js/2.03fdcb24.chunk.js"
+  },
+  {
+    "revision": "f9f938f4c857dbb84110",
+    "url": "/ecommercetv/static/js/main.eb6d8fa7.chunk.js"
+  },
+  {
+    "revision": "d9b286eb7599685fbd93",
+    "url": "/ecommercetv/static/js/runtime-main.c45bfeb5.js"
+  },
+  {
+    "revision": "04e2c662e7ea2d497a438f7afa57d724",
+    "url": "/ecommercetv/static/media/1.04e2c662.jpg"
+  },
+  {
+    "revision": "09c49b5ca18e2e3357990ce47125072b",
+    "url": "/ecommercetv/static/media/1.09c49b5c.jpg"
+  },
+  {
+    "revision": "0af9e3b60aa7614586d85a638ce99d37",
+    "url": "/ecommercetv/static/media/1.0af9e3b6.jpg"
+  },
+  {
+    "revision": "10f06fa17488bc222e5582be41dd5a7f",
+    "url": "/ecommercetv/static/media/1.10f06fa1.jpg"
+  },
+  {
+    "revision": "191c4cfa4a1ccc353299193fdd27240b",
+    "url": "/ecommercetv/static/media/1.191c4cfa.jpg"
+  },
+  {
+    "revision": "1b1b7717bde7c03ee29cfc3194c3d016",
+    "url": "/ecommercetv/static/media/1.1b1b7717.jpg"
+  },
+  {
+    "revision": "1c87c241d2df34b2b67e455138973e99",
+    "url": "/ecommercetv/static/media/1.1c87c241.jpg"
+  },
+  {
+    "revision": "21052209e947722a34a11925028148c9",
+    "url": "/ecommercetv/static/media/1.21052209.jpg"
+  },
+  {
+    "revision": "234dc193bde882b243a62fd811347f39",
+    "url": "/ecommercetv/static/media/1.234dc193.jpg"
+  },
+  {
+    "revision": "385d3caf88b524630646de291c0e7e90",
+    "url": "/ecommercetv/static/media/1.385d3caf.jpg"
+  },
+  {
+    "revision": "43404c4664e54c7154f6e4b6528bc5a8",
+    "url": "/ecommercetv/static/media/1.43404c46.jpg"
+  },
+  {
+    "revision": "5734d484eb234b73e4d60b586d1446bc",
+    "url": "/ecommercetv/static/media/1.5734d484.jpg"
+  },
+  {
+    "revision": "7fa314113c9bb7bf4b4be5918093aaa2",
+    "url": "/ecommercetv/static/media/1.7fa31411.jpg"
+  },
+  {
+    "revision": "81c53a5db9765a87e46ee918349f7217",
+    "url": "/ecommercetv/static/media/1.81c53a5d.jpg"
+  },
+  {
+    "revision": "957d4385cf0d1ba74cb01da0eed8bb2b",
+    "url": "/ecommercetv/static/media/1.957d4385.jpg"
+  },
+  {
+    "revision": "a93043206eb3bda0c42d3639728322e8",
+    "url": "/ecommercetv/static/media/1.a9304320.jpg"
+  },
+  {
+    "revision": "b90dae7279e1ce1afa0334511ac4f83f",
+    "url": "/ecommercetv/static/media/1.b90dae72.jpg"
+  },
+  {
+    "revision": "cad7d9562adc92977657355bbce74057",
+    "url": "/ecommercetv/static/media/1.cad7d956.jpg"
+  },
+  {
+    "revision": "ccec24ee9d9d13151fd8f6752041621f",
+    "url": "/ecommercetv/static/media/1.ccec24ee.jpg"
+  },
+  {
+    "revision": "f85cd76a1db2ab518ce91548694ba3fe",
+    "url": "/ecommercetv/static/media/1.f85cd76a.jpg"
+  },
+  {
+    "revision": "fe1cc5e15f81122cd2f7cc259518d400",
+    "url": "/ecommercetv/static/media/1.fe1cc5e1.jpg"
+  },
+  {
+    "revision": "08aab3a7b539a997c546d2da8a628a22",
+    "url": "/ecommercetv/static/media/2.08aab3a7.jpg"
+  },
+  {
+    "revision": "10abf9e4acdfd7ab587f275b60cc769c",
+    "url": "/ecommercetv/static/media/2.10abf9e4.jpg"
+  },
+  {
+    "revision": "124ea50292245265a8fbdbfb4ec77d2a",
+    "url": "/ecommercetv/static/media/2.124ea502.jpg"
+  },
+  {
+    "revision": "219dc9bc4bcad4c24a9cfbbda5e5c379",
+    "url": "/ecommercetv/static/media/2.219dc9bc.jpg"
+  },
+  {
+    "revision": "23c9f7cd02515bab0b3a9ffaa4323218",
+    "url": "/ecommercetv/static/media/2.23c9f7cd.jpg"
+  },
+  {
+    "revision": "2d58b84b87405a5524ab7f10083da6a5",
+    "url": "/ecommercetv/static/media/2.2d58b84b.jpg"
+  },
+  {
+    "revision": "31fcc018c0414d5db75c36a0c1eedab1",
+    "url": "/ecommercetv/static/media/2.31fcc018.jpg"
+  },
+  {
+    "revision": "3f698f63a2120af4edc817a70d9da715",
+    "url": "/ecommercetv/static/media/2.3f698f63.jpg"
+  },
+  {
+    "revision": "4b2e92e02d06f32b16e77c9f3e1efe7f",
+    "url": "/ecommercetv/static/media/2.4b2e92e0.jpg"
+  },
+  {
+    "revision": "5289a670b1c80e6885c01be861854a30",
+    "url": "/ecommercetv/static/media/2.5289a670.jpg"
+  },
+  {
+    "revision": "61a091882b08feb7017b4e2139d01281",
+    "url": "/ecommercetv/static/media/2.61a09188.jpg"
+  },
+  {
+    "revision": "6d7895882d9506805156d7d5d61deab4",
+    "url": "/ecommercetv/static/media/2.6d789588.jpg"
+  },
+  {
+    "revision": "84720015063f9b3636eb1a4d0805da0d",
+    "url": "/ecommercetv/static/media/2.84720015.jpg"
+  },
+  {
+    "revision": "91ea5546c6ebc7edde6638830da60174",
+    "url": "/ecommercetv/static/media/2.91ea5546.jpg"
+  },
+  {
+    "revision": "9ff9bdfa689974c22d41c5f37048f9a2",
+    "url": "/ecommercetv/static/media/2.9ff9bdfa.jpg"
+  },
+  {
+    "revision": "a28351fdc52b83787cda9f92278b487d",
+    "url": "/ecommercetv/static/media/2.a28351fd.jpg"
+  },
+  {
+    "revision": "a9bb260450c1ac2441d0ff31b4ed74c7",
+    "url": "/ecommercetv/static/media/2.a9bb2604.jpg"
+  },
+  {
+    "revision": "c147f62d0462ea2d459cf72d7f3a7c0b",
+    "url": "/ecommercetv/static/media/2.c147f62d.jpg"
+  },
+  {
+    "revision": "e331e0faa535232ecf6ad272786c7c22",
+    "url": "/ecommercetv/static/media/2.e331e0fa.jpg"
+  },
+  {
+    "revision": "ed352d3bbe53cc3f53b7a16711f26fe2",
+    "url": "/ecommercetv/static/media/2.ed352d3b.jpg"
+  },
+  {
+    "revision": "f680d8a04324d0c4933af905bd20056a",
+    "url": "/ecommercetv/static/media/2.f680d8a0.jpg"
+  },
+  {
+    "revision": "0af9e3b60aa7614586d85a638ce99d37",
+    "url": "/ecommercetv/static/media/3.0af9e3b6.jpg"
+  },
+  {
+    "revision": "18580436559c5e192e6f16835267714f",
+    "url": "/ecommercetv/static/media/3.18580436.jpg"
+  },
+  {
+    "revision": "25c944b4533f09e3b46da9b7742bfb13",
+    "url": "/ecommercetv/static/media/3.25c944b4.jpg"
+  },
+  {
+    "revision": "2d3b4383dcee389ecfa8c5f50a9d9694",
+    "url": "/ecommercetv/static/media/3.2d3b4383.jpg"
+  },
+  {
+    "revision": "39801c87833915e0df5ee4f2c4b98069",
+    "url": "/ecommercetv/static/media/3.39801c87.jpg"
+  },
+  {
+    "revision": "42e8df51d0cd4240b92c5a6c81f93e9b",
+    "url": "/ecommercetv/static/media/3.42e8df51.jpg"
+  },
+  {
+    "revision": "42efc73e44f2715c2dbbf7a8487dc68f",
+    "url": "/ecommercetv/static/media/3.42efc73e.jpg"
+  },
+  {
+    "revision": "4986e291fbbbbb2f28d4d411c7104bfc",
+    "url": "/ecommercetv/static/media/3.4986e291.jpg"
+  },
+  {
+    "revision": "4bda96d0d9d451fd58fceabd8310c726",
+    "url": "/ecommercetv/static/media/3.4bda96d0.jpg"
+  },
+  {
+    "revision": "4d8f1fb114187294aaac9c4ff270fd79",
+    "url": "/ecommercetv/static/media/3.4d8f1fb1.jpg"
+  },
+  {
+    "revision": "742ee67248ee8bc6d488ce2fc1b24c3f",
+    "url": "/ecommercetv/static/media/3.742ee672.jpg"
+  },
+  {
+    "revision": "8d0ea30cb3e59c865c3fe4d9e685d2a6",
+    "url": "/ecommercetv/static/media/3.8d0ea30c.jpg"
+  },
+  {
+    "revision": "93a09ecdd70e0edb20b74822a291521e",
+    "url": "/ecommercetv/static/media/3.93a09ecd.jpg"
+  },
+  {
+    "revision": "ad63da9cd8395ba3cec54a9f3e76e4ce",
+    "url": "/ecommercetv/static/media/3.ad63da9c.jpg"
+  },
+  {
+    "revision": "b0bb84efc2d9d6285e5fbea722bc70d9",
+    "url": "/ecommercetv/static/media/3.b0bb84ef.jpg"
+  },
+  {
+    "revision": "b38090537d2d550c5d6c3e39edbd5432",
+    "url": "/ecommercetv/static/media/3.b3809053.jpg"
+  },
+  {
+    "revision": "bf68fd010a09d3a59cbf3652ee18f7c9",
+    "url": "/ecommercetv/static/media/3.bf68fd01.jpg"
+  },
+  {
+    "revision": "c0fe60eb1ad9feea9cf4a3604cd7077d",
+    "url": "/ecommercetv/static/media/3.c0fe60eb.jpg"
+  },
+  {
+    "revision": "c4a67259b3f93ee1871510c267be595e",
+    "url": "/ecommercetv/static/media/3.c4a67259.jpg"
+  },
+  {
+    "revision": "dae62500e335c107c9df0026b4724fc3",
+    "url": "/ecommercetv/static/media/3.dae62500.jpg"
+  },
+  {
+    "revision": "e332fa82e0f5af60ac79ab61f3e73e03",
+    "url": "/ecommercetv/static/media/3.e332fa82.jpg"
+  },
+  {
+    "revision": "f447d39e00734b88d9febb4bf99dd3a0",
+    "url": "/ecommercetv/static/media/3.f447d39e.jpg"
+  },
+  {
+    "revision": "1f510781ca069935eb7dae538166d854",
+    "url": "/ecommercetv/static/media/4.1f510781.jpg"
+  },
+  {
+    "revision": "4d8f1fb114187294aaac9c4ff270fd79",
+    "url": "/ecommercetv/static/media/4.4d8f1fb1.jpg"
+  },
+  {
+    "revision": "516bde2df0c91ff9b6687a590e5a8738",
+    "url": "/ecommercetv/static/media/4.516bde2d.jpg"
+  },
+  {
+    "revision": "5f09d6c1b95c27459e6baab0e5aa0e95",
+    "url": "/ecommercetv/static/media/4.5f09d6c1.jpg"
+  },
+  {
+    "revision": "6289e4438b210a23c7794a9e6be23995",
+    "url": "/ecommercetv/static/media/4.6289e443.jpg"
+  },
+  {
+    "revision": "7198447dad4cf8a895a43ba44b5ba402",
+    "url": "/ecommercetv/static/media/4.7198447d.jpg"
+  },
+  {
+    "revision": "7aa652758cdd5dfe325c48e68908efec",
+    "url": "/ecommercetv/static/media/4.7aa65275.jpg"
+  },
+  {
+    "revision": "91ea5546c6ebc7edde6638830da60174",
+    "url": "/ecommercetv/static/media/4.91ea5546.jpg"
+  },
+  {
+    "revision": "adb8f9c229caf8a276f1d65c9e6db52e",
+    "url": "/ecommercetv/static/media/4.adb8f9c2.jpg"
+  },
+  {
+    "revision": "b7b84d2d72e53688b6bd9f6122aa379d",
+    "url": "/ecommercetv/static/media/4.b7b84d2d.jpg"
+  },
+  {
+    "revision": "ccec24ee9d9d13151fd8f6752041621f",
+    "url": "/ecommercetv/static/media/4.ccec24ee.jpg"
+  },
+  {
+    "revision": "e87c1b68dddf52739bbd5fc4c48a8a01",
+    "url": "/ecommercetv/static/media/4.e87c1b68.jpg"
+  },
+  {
+    "revision": "fa3f7350d933614f19ffde35c932c168",
+    "url": "/ecommercetv/static/media/4.fa3f7350.jpg"
+  },
+  {
+    "revision": "47e5bbf5d00c1e72cebb78ea0b74e91b",
+    "url": "/ecommercetv/static/media/cart.47e5bbf5.svg"
+  },
+  {
+    "revision": "fc42ae1aad93ec7554fa29f3240a8608",
+    "url": "/ecommercetv/static/media/find_store_img.fc42ae1a.png"
+  },
+  {
+    "revision": "7222a047e2f7e5212676501a0a041cd0",
+    "url": "/ecommercetv/static/media/hdmi_cable.7222a047.jpg"
+  },
+  {
+    "revision": "aa9a964dd6d343f56b21fd420ae15267",
+    "url": "/ecommercetv/static/media/hdmi_gold.aa9a964d.jpg"
+  },
+  {
+    "revision": "a166c7be3cd05fe253cc0dd6926cc610",
+    "url": "/ecommercetv/static/media/pin_card_multi.a166c7be.svg"
+  },
+  {
+    "revision": "70d07804caf4c32e654fb801a4fef1a0",
+    "url": "/ecommercetv/static/media/tv_404.70d07804.jpg"
+  },
+  {
+    "revision": "3d9321e382f07d9f053c114d7cff28c7",
+    "url": "/ecommercetv/static/media/tv_icon.3d9321e3.jpg"
+  },
+  {
+    "revision": "58314679ca4e9f304e93a5e9b612dfcc",
+    "url": "/ecommercetv/static/media/updownarrow.58314679.svg"
+  },
+  {
+    "revision": "511fecbf3e56e5fa5f10323dff1dd08d",
+    "url": "/ecommercetv/static/media/ver_mastercard.511fecbf.svg"
+  },
+  {
+    "revision": "5f92cf0fb165e165d808ba85290f56e6",
+    "url": "/ecommercetv/static/media/ver_visa.5f92cf0f.svg"
+  }
+]);
